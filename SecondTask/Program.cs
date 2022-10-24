80ed029d90330f88cd33aee88d2e585a45d64977
RenatDavletshin <immortal1307@gmail.com>
2022-10-24 16:28:36 +0500

    SecondTaskEnd

---
+++ SecondTask/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять, является ли одно квадратом другого.
+Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+if(numA == numB * numB)
+{
+    Console.WriteLine($"{numA} является квадратом {numB}");
+}
+else
+    if(numB == numA * numA)
+    {
+        Console.WriteLine($"{numB} является квадратом {numA}");
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
