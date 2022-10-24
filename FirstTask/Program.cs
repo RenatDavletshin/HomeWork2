c69de3d534b82d80130ece47af7b1fb985b44e20
RenatDavletshin <immortal1307@gmail.com>
2022-10-24 15:33:57 +0500

    FirstTaskEnd

---
+++ FirstTask/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число, обозначающее день недели. Выяснить, является номер дня недели выходным.
+Console.WriteLine("Введиле номер дня недели (от 1 до 7)");
+int number = int.Parse(Console.ReadLine());
+if(number < 1 || number > 8)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+    if(number > 0 && number < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+}
